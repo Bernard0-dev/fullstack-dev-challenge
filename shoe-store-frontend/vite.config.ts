3d52847b2e775c3e5601392b08572b46bdf9f666
Bernardo <bernard0.dev@proton.me>
2024-12-23 18:17:18 +0000

    Add dotenv

--- shoe-store-frontend/vite.config.ts
+++ shoe-store-frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      API_URL: process.env.VITE_API_URL,
+    },
+  },
 })
